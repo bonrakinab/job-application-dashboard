368511a27f69d3f6b32a84486148a0e072e802ed
Arnob Banik <98481769+bonrakinab@users.noreply.github.com>
2026-08-08 17:12:15 -0400

    Add target companies navigation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ export function Sidebar() {
     <nav className="nav">
       <a href="/">⌂ &nbsp;Dashboard</a>
       <a href="/recommended">★ &nbsp;Recommended Jobs</a>
+      <a href="/companies">◫ &nbsp;Target Companies</a>
       <a href="/applications">◎ &nbsp;Applications</a>
       <a href="/settings">⚙ &nbsp;Settings</a>
     </nav>
