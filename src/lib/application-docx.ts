660daf45e3c4b1dd8477f53f1bf94203d66bd24c
Arnob Banik <98481769+bonrakinab@users.noreply.github.com>
2026-09-09 23:07:28 -0400

    Restore valid DOCX content types and metadata

--- src/lib/application-docx.ts
+++ src/lib/application-docx.ts
@@ -118,7 +118,7 @@ function documentBody(profile: CandidateProfile, pack: ApplicationPack) {
 const CONTENT_TYPES = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
   <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
-  <Default Extension Extension="xml" ContentType="application/xml"/>
+  <Default Extension="xml" ContentType="application/xml"/>
   <Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/>
   <Override PartName="/word/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml"/>
   <Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/>
